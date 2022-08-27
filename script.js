ce179e4dbbcb2c1a74b6a1f81b90bb7f05f1c33d
Elena Sinelle <elle.sinelle@gmail.com>
2022-08-27 17:56:39 +0300

    feat: add shuffle for all ancients

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ azathoth.onclick = function() {
     shubNiggurath.classList.remove('ancient-active');
     selectedAncient = ancientsData[0];
     console.log(selectedAncient);
+    showCard();
 }
 
 cthulthu.onclick = function() {
@@ -32,6 +33,7 @@ cthulthu.onclick = function() {
     shubNiggurath.classList.remove('ancient-active');
     selectedAncient = ancientsData[1];
     console.log(selectedAncient);
+    showCard();
 }
 
 iogSothoth.onclick = function() {
@@ -41,6 +43,7 @@ iogSothoth.onclick = function() {
     shubNiggurath.classList.remove('ancient-active');
     selectedAncient = ancientsData[2];
     console.log(selectedAncient);
+    showCard();
 }
 
 shubNiggurath.onclick = function() {
@@ -50,14 +53,8 @@ shubNiggurath.onclick = function() {
     shubNiggurath.classList.add('ancient-active');
     selectedAncient = ancientsData[3];
     console.log(selectedAncient);
+    showCard();
 }
-
-
-
-
-
-
-
 
 // shuffle 
 
@@ -75,6 +72,7 @@ let greenAzathoth = randomGreen.slice(0, 5);
 let brownAzathoth = randomBrown.slice(0, 9);
 let blueAzathoth = randomBlue.slice(0, 2);
 
+
 let greenFirstAzathoth = greenAzathoth.slice(0, 1);
 let brownFirstAzathoth = brownAzathoth.slice(0, 2);
 let blueFirstAzathoth = blueAzathoth.slice(0, 1);
@@ -85,6 +83,7 @@ let blueSecondAzathoth = blueAzathoth.slice(1);
 
 let greenThirdAzathoth = greenAzathoth.slice(3);
 let brownThirdAzathoth = brownAzathoth.slice(5);
+
 
 let firstAzathoth = [...greenFirstAzathoth, ...brownFirstAzathoth, ...blueFirstAzathoth];
 let randomFirstAzathoth = shuffle(firstAzathoth);
@@ -97,17 +96,125 @@ let randomThirdAzathoth = shuffle(thirdAzathoth);
 
 let randomAzathoth = [...randomFirstAzathoth, ...randomSecondAzathoth, ...randomThirdAzathoth];
 
+// shuffle for Cthulthu
+
+let greenCthulthu = randomGreen.slice(0, 4);
+let brownCthulthu = randomBrown.slice(0, 9);
+let blueCthulthu = randomBlue.slice(0, 2);
 
 
-// show cards for Azathoth
+let brownFirstCthulthu = brownCthulthu.slice(0, 2);
+let blueFirstCthulthu = blueCthulthu.slice(0, 2);
+
+let greenSecondCthulthu = greenCthulthu.slice(0, 1);
+let brownSecondCthulthu = brownCthulthu.slice(2, 5);
+
+let greenThirdCthulthu = greenCthulthu.slice(1);
+let brownThirdCthulthu = brownCthulthu.slice(5);
+
+
+let firstCthulthu = [...brownFirstCthulthu, ...blueFirstCthulthu];
+let randomFirstCthulthu = shuffle(firstCthulthu);
+
+let secondCthulthu = [...greenSecondCthulthu, ...brownSecondCthulthu];
+let randomSecondCthulthu = shuffle(secondCthulthu);
+
+let thirdCthulthu = [...greenThirdCthulthu, ...brownThirdCthulthu];
+let randomThirdCthulthu = shuffle(thirdCthulthu);
+
+let randomCthulthu = [...randomFirstCthulthu, ...randomSecondCthulthu, ...randomThirdCthulthu];
+
+// shuffle for IogSothoth
+
+let greenIogSothoth = randomGreen.slice(0, 5);
+let brownIogSothoth = randomBrown.slice(0, 9);
+let blueIogSothoth = randomBlue.slice(0, 2);
+
+
+let brownFirstIogSothoth = brownIogSothoth.slice(0, 2);
+let blueFirstIogSothoth = blueIogSothoth.slice(0, 1);
+
+let greenSecondIogSothoth = greenIogSothoth.slice(0, 2);
+let brownSecondIogSothoth = brownIogSothoth.slice(2, 5);
+let blueSecondIogSothoth = blueIogSothoth.slice(1, 2);
+
+let greenThirdIogSothoth = greenIogSothoth.slice(2);
+let brownThirdIogSothoth = brownIogSothoth.slice(5);
+
+
+let firstIogSothoth = [...brownFirstIogSothoth, ...blueFirstIogSothoth];
+let randomFirstIogSothoth = shuffle(firstIogSothoth);
+
+let secondIogSothoth = [...greenSecondIogSothoth, ...brownSecondIogSothoth, ...blueSecondIogSothoth];
+let randomSecondIogSothoth = shuffle(secondIogSothoth);
+
+let thirdIogSothoth = [...greenThirdIogSothoth, ...brownThirdIogSothoth];
+let randomThirdIogSothoth = shuffle(thirdIogSothoth);
+
+let randomIogSothoth = [...randomFirstIogSothoth, ...randomSecondIogSothoth, ...randomThirdIogSothoth];
+
+
+// shuffle for ShubNiggurath
+
+let greenShubNiggurath = randomGreen.slice(0, 6);
+let brownShubNiggurath = randomBrown.slice(0, 8);
+let blueShubNiggurath = randomBlue.slice(0, 2);
+
+
+let greenFirstShubNiggurath = greenShubNiggurath.slice(0, 1);
+let brownFirstShubNiggurath = brownShubNiggurath.slice(0, 2);
+let blueFirstShubNiggurath = blueShubNiggurath.slice(0, 1);
+
+let greenSecondShubNiggurath = greenShubNiggurath.slice(1, 4);
+let brownSecondShubNiggurath = brownShubNiggurath.slice(2, 4);
+let blueSecondShubNiggurath = blueShubNiggurath.slice(1, 2);
+
+let greenThirdShubNiggurath = greenShubNiggurath.slice(4);
+let brownThirdShubNiggurath = brownShubNiggurath.slice(4);
+
+
+let firstShubNiggurath = [...greenFirstShubNiggurath, ...brownFirstShubNiggurath, ...blueFirstShubNiggurath];
+let randomFirstShubNiggurath = shuffle(firstShubNiggurath);
+
+let secondShubNiggurath = [...greenSecondShubNiggurath, ...brownSecondShubNiggurath, ...blueSecondShubNiggurath];
+let randomSecondShubNiggurath = shuffle(secondShubNiggurath);
+
+let thirdShubNiggurath = [...greenThirdShubNiggurath, ...brownThirdShubNiggurath];
+let randomThirdShubNiggurath = shuffle(thirdShubNiggurath);
+
+let randomShubNiggurath = [...randomFirstShubNiggurath, ...randomSecondShubNiggurath, ...randomThirdShubNiggurath];
+
+// show cards
+
 let card = document.querySelector('.card');
+let buttonCard = document.querySelector('.show-card');
 
 function showCard() {
-    console.log(randomAzathoth);
-    card.src = randomAzathoth[0].cardFace;
-    randomAzathoth.shift();
+    if (selectedAncient == ancientsData[0]) {
+        console.log(randomAzathoth);
+        card.src = randomAzathoth[0].cardFace;
+        randomAzathoth.shift();
+        if (randomAzathoth.length == 0) alert('No cards left for Azathoth');
+    } else if (selectedAncient == ancientsData[1]) {
+        console.log(randomCthulthu);
+        card.src = randomCthulthu[0].cardFace;
+        randomCthulthu.shift();
+        if (randomCthulthu.length == 0) alert('No cards left for Cthulthu');
+    } else if (selectedAncient == ancientsData[2]) {
+        console.log(randomIogSothoth);
+        card.src = randomIogSothoth[0].cardFace;
+        randomIogSothoth.shift();
+        if (randomIogSothoth.length == 0) alert('No cards left for IogSothoth');
+    } else if (selectedAncient == ancientsData[3]) {
+        console.log(randomShubNiggurath);
+        card.src = randomShubNiggurath[0].cardFace;
+        randomShubNiggurath.shift();
+        if (randomShubNiggurath.length == 0) alert('No cards left for ShubNiggurath');
+    }
 }
-card.addEventListener('click', showCard);
+   
+buttonCard.addEventListener('click', showCard);
+
 
 // counter
 
