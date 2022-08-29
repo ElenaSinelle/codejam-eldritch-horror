ae8ba1b42e1ed8ee782ce719fb1c3f0805df8261
ElenaSinelle <elle.sinelle@gmail.com>
2022-08-29 17:42:02 +0300

    Update index.js

--- data/mythicCards/brown/index.js
+++ data/mythicCards/brown/index.js
@@ -3,127 +3,127 @@
 const cardsData = [
   {
     id: 'brown1',
-    cardFace: '../assets/MythicCards/brown/brown1.webp',
+    cardFace: './assets/MythicCards/brown/brown1.webp',
     difficulty: 'normal',
     color:'brown'
   },
   {
     id: 'brown2',
-    cardFace: '../assets/MythicCards/brown/brown2.webp',
+    cardFace: './assets/MythicCards/brown/brown2.webp',
     difficulty: 'normal',
     color:'brown'
   },
   {
     id: 'brown3',
-    cardFace: '../assets/MythicCards/brown/brown3.webp',
+    cardFace: './assets/MythicCards/brown/brown3.webp',
     difficulty: 'normal',
     color:'brown'
   },
   {
     id: 'brown4',
-    cardFace: '../assets/MythicCards/brown/brown4.webp',
+    cardFace: './assets/MythicCards/brown/brown4.webp',
     difficulty: 'normal',
     color:'brown'
   },
   {
     id: 'brown5',
-    cardFace: '../assets/MythicCards/brown/brown5.webp',
+    cardFace: './assets/MythicCards/brown/brown5.webp',
     difficulty: 'normal',
     color:'brown'
   },
   {
     id: 'brown6',
-    cardFace: '../assets/MythicCards/brown/brown6.webp',
+    cardFace: './assets/MythicCards/brown/brown6.webp',
     difficulty: 'hard',
     color:'brown'
   },
   {
     id: 'brown7',
-    cardFace: '../assets/MythicCards/brown/brown7.webp',
+    cardFace: './assets/MythicCards/brown/brown7.webp',
     difficulty: 'hard',
     color:'brown'
   },
   {
     id: 'brown8',
-    cardFace: '../assets/MythicCards/brown/brown8.webp',
+    cardFace: './assets/MythicCards/brown/brown8.webp',
     difficulty: 'hard',
     color:'brown'
   },
   {
     id: 'brown9',
-    cardFace: '../assets/MythicCards/brown/brown9.webp',
+    cardFace: './assets/MythicCards/brown/brown9.webp',
     difficulty: 'hard',
     color:'brown'
   },
   {
     id: 'brown10',
-    cardFace: '../assets/MythicCards/brown/brown10.webp',
+    cardFace: './assets/MythicCards/brown/brown10.webp',
     difficulty: 'hard',
     color:'brown'
   },
   {
     id: 'brown11',
-    cardFace: '../assets/MythicCards/brown/brown11.webp',
+    cardFace: './assets/MythicCards/brown/brown11.webp',
     difficulty: 'easy',
     color:'brown'
   },
   {
     id: 'brown12',
-    cardFace: '../assets/MythicCards/brown/brown12.webp',
+    cardFace: './assets/MythicCards/brown/brown12.webp',
     difficulty: 'easy',
     color:'brown'
   },
   {
     id: 'brown13',
-    cardFace: '../assets/MythicCards/brown/brown13.webp',
+    cardFace: './assets/MythicCards/brown/brown13.webp',
     difficulty: 'easy',
     color:'brown'
   },
   {
     id: 'brown14',
-    cardFace: '../assets/MythicCards/brown/brown14.webp',
+    cardFace: './assets/MythicCards/brown/brown14.webp',
     difficulty: 'easy',
     color:'brown'
   },
   {
     id: 'brown15',
-    cardFace: '../assets/MythicCards/brown/brown15.webp',
+    cardFace: './assets/MythicCards/brown/brown15.webp',
     difficulty: 'normal',
     color:'brown'
   },
   {
     id: 'brown16',
-    cardFace: '../assets/MythicCards/brown/brown16.webp',
+    cardFace: './assets/MythicCards/brown/brown16.webp',
     difficulty: 'normal',
     color:'brown'
   },
   {
     id: 'brown17',
-    cardFace: '../assets/MythicCards/brown/brown17.webp',
+    cardFace: './assets/MythicCards/brown/brown17.webp',
     difficulty: 'normal',
     color:'brown'
   },
   {
     id: 'brown18',
-    cardFace: '../assets/MythicCards/brown/brown18.webp',
+    cardFace: './assets/MythicCards/brown/brown18.webp',
     difficulty: 'normal',
     color:'brown'
   },
   {
     id: 'brown19',
-    cardFace: '../assets/MythicCards/brown/brown19.webp',
+    cardFace: './assets/MythicCards/brown/brown19.webp',
     difficulty: 'normal',
     color:'brown'
   },
   {
     id: 'brown20',
-    cardFace: '../assets/MythicCards/brown/brown20.webp',
+    cardFace: './assets/MythicCards/brown/brown20.webp',
     difficulty: 'normal',
     color:'brown'
   },
   {
     id: 'brown21',
-    cardFace: '../assets/MythicCards/brown/brown21.webp',
+    cardFace: './assets/MythicCards/brown/brown21.webp',
     difficulty: 'easy',
     color:'brown'
   },
