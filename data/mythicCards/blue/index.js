27e131940a5c5c6e855f4df7ad6c2e93a692fbac
ElenaSinelle <elle.sinelle@gmail.com>
2022-08-29 17:08:31 +0300

    Update index.js

--- data/mythicCards/blue/index.js
+++ data/mythicCards/blue/index.js
@@ -3,73 +3,73 @@
 const cardsData = [
   {
     id: 'blue1',
-    cardFace: './assets/MythicCards/blue/blue1.webp',
+    cardFace: '/assets/MythicCards/blue/blue1.webp',
     difficulty: 'hard',
     color:'blue'
   },
   {
     id: 'blue2',
-    cardFace: './assets/MythicCards/blue/blue2.webp',
+    cardFace: '/assets/MythicCards/blue/blue2.webp',
     difficulty: 'hard',
     color:'blue'
   },
   {
     id: 'blue3',
-    cardFace: './assets/MythicCards/blue/blue3.webp',
+    cardFace: '/assets/MythicCards/blue/blue3.webp',
     difficulty: 'easy',
     color:'blue'
   },
   {
     id: 'blue4',
-    cardFace: './assets/MythicCards/blue/blue4.webp',
+    cardFace: '/assets/MythicCards/blue/blue4.webp',
     difficulty: 'easy',
     color:'blue'
   },
   {
     id: 'blue5',
-    cardFace: './assets/MythicCards/blue/blue5.webp',
+    cardFace: '/assets/MythicCards/blue/blue5.webp',
     difficulty: 'easy',
     color:'blue'
   },
   {
     id: 'blue6',
-    cardFace: './assets/MythicCards/blue/blue6.webp',
+    cardFace: '/assets/MythicCards/blue/blue6.webp',
     difficulty: 'hard',
     color:'blue'
   },
   {
     id: 'blue7',
-    cardFace: './assets/MythicCards/blue/blue7.webp',
+    cardFace: '/assets/MythicCards/blue/blue7.webp',
     difficulty: 'normal',
     color:'blue'
   },
   {
     id: 'blue8',
-    cardFace: './assets/MythicCards/blue/blue8.webp',
+    cardFace: '/assets/MythicCards/blue/blue8.webp',
     difficulty: 'hard',
     color:'blue'
   },
   {
     id: 'blue9',
-    cardFace: './assets/MythicCards/blue/blue9.webp',
+    cardFace: /assets/MythicCards/blue/blue9.webp',
     difficulty: 'normal',
     color:'blue'
   },
   {
     id: 'blue10',
-    cardFace: './assets/MythicCards/blue/blue10.webp',
+    cardFace: '/assets/MythicCards/blue/blue10.webp',
     difficulty: 'easy',
     color:'blue'
   },
   {
     id: 'blue11',
-    cardFace: './assets/MythicCards/blue/blue11.webp',
+    cardFace: '/assets/MythicCards/blue/blue11.webp',
     difficulty: 'normal',
     color:'blue'
   },
   {
     id: 'blue12',
-    cardFace: './assets/MythicCards/blue/blue12.webp',
+    cardFace: '/assets/MythicCards/blue/blue12.webp',
     difficulty: 'normal',
     color:'blue'
   },
